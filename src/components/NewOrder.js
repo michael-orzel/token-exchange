e6eb2b5d493b549f5a78b66103168f64cc0aa4d6
Michael Orzel <hello@michaelorzel.com>
2022-01-24 15:59:57 -0500

    typo

--- src/components/NewOrder.js
+++ src/components/NewOrder.js
@@ -96,7 +96,7 @@ const showForm = (props) => {
             <div className="input-group">
               <input
                 type="text"
-                placeholder="Sel Price"
+                placeholder="Sell Price"
                 onChange={(e) => dispatch( sellOrderPriceChanged(e.target.value) )}
                 className="form-control form-control-sm bg-dark text-white"
                 required
